96233a2153c7afb71a434a627a0c5d8001f42797
Bethanii <BethaniHampton@gmail.com>
2021-04-18 19:30:44 -0500

    second updated commit

--- TaskManager/Form1.Designer.cs
+++ TaskManager/Form1.Designer.cs
@@ -29,13 +29,118 @@ namespace TaskManager
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.taskItemBox = new System.Windows.Forms.CheckedListBox();
+            this.weekDayBox = new System.Windows.Forms.CheckedListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBoxResults = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // taskItemBox
+            // 
+            this.taskItemBox.Font = new System.Drawing.Font("Microsoft YaHei UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.taskItemBox.FormattingEnabled = true;
+            this.taskItemBox.Items.AddRange(new object[] {
+            "Do laundry",
+            "Get groceries ",
+            "Taxes ",
+            "Do dishes ",
+            "Respond to emails",
+            "Make vacation checklist",
+            "Get vehicle oil change",
+            "Mop floors",
+            "Water plants",
+            "Call grandma"});
+            this.taskItemBox.Location = new System.Drawing.Point(447, 102);
+            this.taskItemBox.Name = "taskItemBox";
+            this.taskItemBox.Size = new System.Drawing.Size(240, 220);
+            this.taskItemBox.TabIndex = 0;
+            // 
+            // weekDayBox
+            // 
+            this.weekDayBox.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.weekDayBox.FormattingEnabled = true;
+            this.weekDayBox.Items.AddRange(new object[] {
+            "Monday",
+            "Tuesday",
+            "Wednesday",
+            "Thursday",
+            "Friday",
+            "Saturday",
+            "Sunday"});
+            this.weekDayBox.Location = new System.Drawing.Point(118, 111);
+            this.weekDayBox.Name = "weekDayBox";
+            this.weekDayBox.Size = new System.Drawing.Size(232, 200);
+            this.weekDayBox.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.LightSalmon;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(136, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(175, 24);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Chose Day of Week";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.LightSalmon;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(510, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(105, 24);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Chose Task";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Pink;
+            this.button1.Font = new System.Drawing.Font("Microsoft YaHei UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(264, 381);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(271, 34);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Get Weekly Schedule";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBoxResults
+            // 
+            this.textBoxResults.Location = new System.Drawing.Point(94, 381);
+            this.textBoxResults.Name = "textBoxResults";
+            this.textBoxResults.Size = new System.Drawing.Size(100, 22);
+            this.textBoxResults.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBoxResults);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.weekDayBox);
+            this.Controls.Add(this.taskItemBox);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.CheckedListBox taskItemBox;
+        private System.Windows.Forms.CheckedListBox weekDayBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBoxResults;
     }
 }
 
--- TaskManager/Form1.cs
+++ TaskManager/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,107 @@ namespace TaskManager
         public Form1()
         {
             InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            weekDayBox.Items.AddRange(new object[]
+            {
+                "Monday",
+                "Tuesday",
+                "Wednesday",
+                "Thursday",
+                "Friday",
+                "Saturday",
+                "Sunday",
+            });
+
+            taskItemBox.Items.AddRange(new object[]
+            {
+                56.3,
+                2,
+                44.77,
+                32.6548,
+                87.0,
+                47.999,
+                34.6,
+                75.89,
+                88.899,
+                466,
+                1,
+                34,
+                45,
+                33.3333333,
+                6.987654,
+                5
+            });
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int weekDay = weekDayBox.CheckedItems.Count;
+            string[] w = new string[weekDay];
+            int selectedIndex = 0;
+            string buildString = "Week day:\r\n";
+
+            foreach (var item in weekDayBox.CheckedItems)
+            {
+                w[selectedIndex] = item.ToString();
+                buildString += w[selectedIndex] + "\r\n";
+                ++selectedIndex;
+            }
+
+            int taskItem = taskItemBox.CheckedItems.Count;
+            string[] task = new string[taskItem];
+            selectedIndex = 0;
+            buildString += "\r\nTasks assigned for the day: \r\n";
+
+            foreach (var item in taskItemBox.CheckedItems)
+            {
+                task[selectedIndex] = item.ToString();
+                buildString += task[selectedIndex] + "\r\n";
+                ++selectedIndex;
+            }
+
+            MessageBox.Show(buildString);
+            textBoxResults.Text = buildString;
+
+            string filePath = System.Environment.GetEnvironmentVariable("USERPROFILE") + "\\Tasks.txt";
+            string strRead;
+
+            FileStream fs = new FileStream(filePath, FileMode.Open, FileAccess.Read);
+            StreamReader reader = new StreamReader(fs);
+
+            int count;
+            count = GetCount(filePath);
+
+            for (int counter = 1; counter < count; ++counter)
+            {
+                strRead = reader.ReadLine();
+                taskItemBox.Items.Add(strRead);
+            }
+
+            reader.Close();
+            fs.Close();
+        }
+        static int GetCount(string file)
+        {
+            int count = 0; 
+            string strRead;
+
+            FileStream sr = new FileStream(file, FileMode.Open, FileAccess.Read);
+            StreamReader readerCount = new StreamReader(sr);
+
+            strRead = readerCount.ReadLine();  
+            while (strRead != null)
+            {
+                ++count;
+                strRead = readerCount.ReadLine();               
+            }
+
+            readerCount.Close();
+            sr.Close();
+
+            return count;
         }
     }
 }
